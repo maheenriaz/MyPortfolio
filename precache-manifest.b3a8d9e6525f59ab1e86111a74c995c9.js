2d8a30f69669b0faa660a2f49ae699292889dbd7
maheenriaz <mahiriaz99@gmail.com>
2020-05-06 05:55:43 -0700

    Updates

---
+++ precache-manifest.b3a8d9e6525f59ab1e86111a74c995c9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b9b1a9e1f67fcf5e596a5b9cb465d6a",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "0dba773184d8feb13e7e",
+    "url": "/MyPortfolio/static/css/main.4c4f7dda.chunk.css"
+  },
+  {
+    "revision": "6a066968a0eb36276a64",
+    "url": "/MyPortfolio/static/js/2.3d9a5b8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MyPortfolio/static/js/2.3d9a5b8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dba773184d8feb13e7e",
+    "url": "/MyPortfolio/static/js/main.d56c410d.chunk.js"
+  },
+  {
+    "revision": "3cfeb892894e7257251c",
+    "url": "/MyPortfolio/static/js/runtime-main.9b4b2915.js"
+  },
+  {
+    "revision": "68b835f86d9402568aa41a2946798246",
+    "url": "/MyPortfolio/static/media/Pic.68b835f8.jpg"
+  },
+  {
+    "revision": "d8615ced86c9a1c57429aac484a994a2",
+    "url": "/MyPortfolio/static/media/location.d8615ced.jpg"
+  },
+  {
+    "revision": "946dcb11d95de84f96cb0c4417927fd5",
+    "url": "/MyPortfolio/static/media/myimg.946dcb11.jpeg"
+  },
+  {
+    "revision": "621eda6e5c94a1925da4d2f8ff6f6f78",
+    "url": "/MyPortfolio/static/media/reducedcost.621eda6e.jpg"
+  },
+  {
+    "revision": "f5a65275beb6671ab3520f9224175543",
+    "url": "/MyPortfolio/static/media/scalibilityicon.f5a65275.jpg"
+  }
+]);
